1395d84e6b62b7f4b506ea7e91d87a1b33473cfb
Artyom Pstygo <apstygo@gmail.com>
2019-08-22 14:19:13 +0400

    Revamp Scrollable protocol, now it is does not depend on UIScrollViewDelegate. Also rename enclosing folder

---
+++ Sources/sheets/Scrollable/Scrollable.swift
@@ -0,0 +1,22 @@
+//
+//  Scrollable.swift
+//  sheets
+//
+//  Created by Artyom Pstygo on 15.08.2019.
+//  Copyright © 2019 Artyom Pstygo. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+public protocol ScrollableDelegate: class {
+    func scrollableDidScroll(_ scrollView: UIScrollView)
+    func scrollableWillBeginDragging(_ scrollView: UIScrollView)
+    func scrollableWillEndDragging(_ scrollView: UIScrollView,
+                                   withVelocity velocity: CGPoint,
+                                   targetContentOffset: UnsafeMutablePointer<CGPoint>)
+}
+
+public protocol Scrollable: class {
+    var delegate: ScrollableDelegate? { get set }
+}
